7db5d47d055c18ab1fc2edbc6af5dd850f73a2fe
Kinglesley <lesleyvdpol@gmail.com>
2013-11-19 13:26:00 +0100

    Added getPassengers, returns List of Passengers

--- LuggageTrackerTool2/src/connectivity/QueryManager.java
+++ LuggageTrackerTool2/src/connectivity/QueryManager.java
@@ -3,6 +3,8 @@ package connectivity;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import model.Passenger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -20,22 +22,26 @@ public class QueryManager {
         this.databaseManager = databaseManager;
     }
 
+    /**
+     * Methode om passagiers aan te maken
+     * @param passengerId
+     * @return 
+     */
     public Passenger getPassenger(int passengerId) {
         Passenger passenger = new Passenger();
         try {
             String sql = "SELECT * FROM passenger WHERE passenger_id='" + passengerId + "'";
             ResultSet result = databaseManager.doQuery(sql);
             if (result.next()) {
-                passenger = new Passenger(
-                        result.getString("passenger_id"),
-                        result.getString(sql),
-                        result.getString(sql),
-                        result.getString(sql),
-                        result.getString(sql),
-                        result.getString(sql),
-                        result.getString(sql),
-                        result.getString(sql)
-                );
+                passenger = new Passenger( // Dit werkt nog niet, kolommen zijn nog niet compleet
+                        result.getString("Passenger ID"),
+                        result.getString("Name"),
+                        result.getString("Insertion"),
+                        result.getString("Surname"),
+                        result.getString("Gender"),
+                        result.getString("Date of birth"),
+                        result.getString("Mobile phone"),
+                        result.getString("Home phone"));
             }
         } catch (SQLException e) {
             System.err.println(DatabaseManager.SQL_EXCEPTION + e.getMessage());
@@ -43,4 +49,30 @@ public class QueryManager {
         return passenger;
     }
 
+    /**
+     * Returned alle passagiers als een list
+     * @return 
+     */
+    public List<Passenger> getPassengers() {
+        List<Passenger> passengers = new ArrayList<Passenger>();
+
+        try {
+            String sql = "SELECT * FROM passenger";
+            ResultSet result = databaseManager.doQuery(sql);
+            while (result.next()) {
+                passengers.add(new Passenger( // Dit werkt nog niet, kolommen zijn nog niet compleet
+                        result.getString("Passenger ID"),
+                        result.getString("Name"),
+                        result.getString("Insertion"),
+                        result.getString("Surname"),
+                        result.getString("Gender"),
+                        result.getString("Date of birth"),
+                        result.getString("Mobile phone"),
+                        result.getString("Home phone")));
+            }
+        } catch (SQLException e) {
+            System.err.println(DatabaseManager.SQL_EXCEPTION + e.getMessage());
+        }
+        return passengers;
+    }
 }
