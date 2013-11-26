a92c50f93a832593c387be473fa0543ce9305455
TomasSlaman <tomas1993@msn.com>
2013-11-26 12:35:28 +0100

    Added queries according to CRUD without 'U'
    Delete passenger doesn't automatically delete the existing Address
    entries yet.

--- LuggageTrackerTool2/src/connectivity/QueryManager.java
+++ LuggageTrackerTool2/src/connectivity/QueryManager.java
@@ -23,6 +23,69 @@ public class QueryManager {
     }
 
     /**
+     * Function to create a new passenger row in DB
+     *
+     * @param passenger
+     * @param address
+     */
+    public void addPassenger(Passenger passenger, Address address) {
+        String sql_passengerTable = "INSERT INTO `passenger` (Passenger ID, Surname, Name, Gender, Date of birth, Mobile phone, Home phone, Home address ID, Temporary address ID,  Insertion)"
+                + "VALUES:(" + passenger.getPassengerId() + ", `" + passenger.getSurname()
+                + "`, `" + passenger.getFirstName() + "`, `" + passenger.getGender() + "`, `"
+                + passenger.getDateOfBirth() + "`, `" + passenger.getMobileNumber() + "`, `"
+                + passenger.getPrivateNumber() + "`, " + passenger.getHomeAddressId() + ", "
+                + passenger.getTemporaryAddressId() + ", `" + passenger.getTussenvoegsel() + "`)";
+
+        //Column address ID needs to be handled in the database, should this auto-increment?
+        this.databaseManager.insertQuery(sql_passengerTable);
+        this.addAddress(address);
+    }
+
+    /**
+     * Function to search passenger in DB.
+     *
+     * @param passengerId
+     * @return
+     */
+    public Passenger getPassenger(int passengerId) {
+        Passenger passenger = new Passenger();
+        try {
+            String sql = "SELECT * FROM passenger WHERE `passenger id`='" + passengerId + "'";
+            ResultSet result = databaseManager.doQuery(sql);
+            if (result.next()) {
+                passenger = new Passenger(
+                        result.getInt("Passenger ID"),
+                        result.getString("Surname"),
+                        result.getString("Name"),
+                        result.getString("Gender"),
+                        result.getDate("Date of birth"),
+                        result.getString("Mobile phone"),
+                        result.getString("Home phone"),
+                        result.getInt("Home address ID"),
+                        result.getInt("Temporary address ID"),
+                        result.getString("Insertion"));
+            }
+        } catch (SQLException e) {
+            System.err.println(DatabaseManager.SQL_EXCEPTION + e.getMessage());
+        }
+        return passenger;
+    }
+
+    /**
+     * Deletes passenger does NOT delete addresses, yet.
+     *
+     * @param passengerId
+     */
+    public void deletePassenger(int passengerId) {
+        this.getPassenger(passengerId);
+        String sql = "DELETE FROM `Passenger` WHERE `Passenger ID` = `" + passengerId + "`";
+        this.databaseManager.insertQuery(sql);
+
+        // Do temporary address and home address ALWAYS get a value? 
+        // Or does this get set to -1 or something similar if no values are inserted for address?
+    }
+
+    /**
      * Returned alle passagiers als een list
      *
      * @return
@@ -52,57 +115,63 @@ public class QueryManager {
         return passengers;
     }
 
-    //addPassenger not done yet.
-    public void addPassenger(Passenger passenger) {
-        String sql_passengerTable = "INSERT INTO `passenger` (Passenger ID, Surname, Name, Gender, Date of birth, Mobile phone, Home phone, Home address ID, Temporary address ID,  Insertion)"
-                + "VALUES:(" + passenger.getPassengerId() + ", `" + passenger.getSurname()
-                + "`, `" + passenger.getFirstName() + "`, `" + passenger.getGender() + "`, `"
-                + passenger.getDateOfBirth() + "`, `" + passenger.getMobileNumber() + "`, `"
-                + passenger.getPrivateNumber() + "`, " + passenger.getHomeAddressId() + ", "
-                + passenger.getTemporaryAddressId() + ", `" + passenger.getTussenvoegsel() + "`)";
+    /**
+     * Adds an address
+     *
+     * @param address
+     */
+    public void addAddress(Address address) {
+        String sql = "INSERT INTO `Address` (Address ID, Street, Street Number, Zipcode, City, Country)"
+                + "VALUES: (" + address.getAddressId() + ", `" + address.getStreetName()
+                + "`, " + address.getStreetNumber() + ", `" + address.getZipCode()
+                + "`, `" + address.getCity() + "`, `" + address.getCountry() + "`)";
 
-        //Column address ID needs to be handled in the database, should this auto-increment?
-        //String sql_addressId = "INSERT INTO `address` (Street, Street Number, Postcode, City, Country)" 
-        //        + "VALUES:`";
-        this.databaseManager.insertQuery(sql_passengerTable);
-        //this.databaseManager.insertQuery(sql_addressId);
-
+        this.databaseManager.insertQuery(sql);
     }
 
     /**
-     * Methode om passagiers aan te maken
      *
-     * @param passengerId
-     * @return
+     * @return List of all addresses
      */
-    public Passenger getPassenger(int passengerId) {
-        Passenger passenger = new Passenger();
+    public List<Address> getAddresses() {
+        List<Address> addresses = new ArrayList<Address>();
+
         try {
-            String sql = "SELECT * FROM passenger WHERE `passenger id`='" + passengerId + "'";
+            String sql = "SELECT * FROM Address";
             ResultSet result = databaseManager.doQuery(sql);
-            if (result.next()) {
-                passenger = new Passenger(
-                        result.getInt("Passenger ID"),
-                        result.getString("Surname"),
-                        result.getString("Name"),
-                        result.getString("Gender"),
-                        result.getDate("Date of birth"),
-                        result.getString("Mobile phone"),
-                        result.getString("Home phone"),
-                        result.getInt("Home address ID"),
-                        result.getInt("Temporary address ID"),
-                        result.getString("Insertion"));
+            while (result.next()) {
+                addresses.add(new Address(result.getInt("Address ID"),
+                        result.getString("Street"),
+                        result.getInt("Street Number"),
+                        result.getString("Zipcode"),
+                        result.getString("City"),
+                        result.getString("Country")));
             }
         } catch (SQLException e) {
             System.err.println(DatabaseManager.SQL_EXCEPTION + e.getMessage());
         }
-        return passenger;
+        return addresses;
     }
 
+    /**
+     * Deletes address with given ID.
+     *
+     * @param addressId
+     */
+    public void deleteAddress(int addressId) {
+        String sql = "DELETE FROM `Address` WHERE `Address ID` = `" + addressId + "`";
+        this.databaseManager.insertQuery(sql);
+    }
+
+    /**
+     *
+     * @param addressId
+     * @return address requested
+     */
     public Address getAddress(int addressId) {
         Address address = new Address();
         try {
-            String sql = "SELECT * FROM Address";
+            String sql = "SELECT * FROM Address WHERE Address ID = '" + addressId + "'";
             ResultSet result = databaseManager.doQuery(sql);
             if (result.next()) {
                 address = new Address(result.getInt("Address ID"),
@@ -117,5 +186,117 @@ public class QueryManager {
         }
 
         return address;
+    }
+
+    /**
+     * adds luggage to DB.
+     *
+     * @param luggage
+     */
+    public void addLuggage(Luggage luggage) {
+        String sql = "INSERT INTO `Luggage` (Luggage ID, Description, Storage Location, Luggage Status)"
+                + "VALUES: (" + luggage.getLuggageId() + ", `" + luggage.getDescription() + "`, `" + luggage.getStorageLocation() + "`, `" + luggage.getStatus() + "`)";
+        this.databaseManager.insertQuery(sql);
+    }
+
+    /**
+     * Gets luggage.
+     *
+     * @param luggageId
+     * @return
+     */
+    public Luggage getLuggage(int luggageId) {
+        Luggage luggage = new Luggage();
+        try {
+            String sql = "SELECT * FROM Luggage WHERE Luggage ID = `" + luggageId + "`";
+            ResultSet result = databaseManager.doQuery(sql);
+            if (result.next()) {
+                luggage = new Luggage(result.getInt("Luggage ID"),
+                        result.getString("Description"),
+                        result.getString("Storage Location"),
+                        result.getString("Luggage Status"));
+            }
+        } catch (SQLException e) {
+            System.err.println(DatabaseManager.SQL_EXCEPTION + e.getMessage());
+        }
+
+        return luggage;
+    }
+
+    /**
+     *
+     * @param luggageId
+     */
+    public void deleteLuggage(int luggageId) {
+        String sql = "DELETE FROM `Luggage` WHERE `Luggage ID` = `" + luggageId + "`";
+        this.databaseManager.insertQuery(sql);
+    }
+
+    /**
+     * Gets a list which includes every piece of luggage.
+     *
+     * @return
+     */
+    public List<Luggage> getLuggages() {
+        List<Luggage> luggages = new ArrayList<Luggage>();
+
+        try {
+            String sql = "SELECT * FROM Luggage";
+            ResultSet result = databaseManager.doQuery(sql);
+            while (result.next()) {
+                luggages.add(new Luggage(result.getInt("Luggage ID"),
+                        result.getString("Description"),
+                        result.getString("Storage Location"),
+                        result.getString("Luggage Status")));
+            }
+        } catch (SQLException e) {
+            System.err.println(DatabaseManager.SQL_EXCEPTION + e.getMessage());
+        }
+
+        return luggages;
+    }
+
+    /** Add a user.
+     * 
+     * @param user 
+     */
+    public void addUser(User user) {
+        String sql = "INSERT INTO `User` (Username, Password, Privileges)" + 
+                "VALUES: (`" + user.getUsername() + "`, `" + user.getPassword() + "`, " + user.getRights() + ")";
+        
+        this.databaseManager.insertQuery(sql);
+    }
+
+    /** Get a user out of the database by username
+     * 
+     * @param username
+     * @return 
+     */
+    public User getUserByName(String username) {
+        User user = new User();
+        try {
+            String sql = "SELECT * FROM `User` WHERE `Username` = `" + username + "`";
+            ResultSet result = databaseManager.doQuery(sql);
+            if (result.next()) {
+                user = new User(result.getString("Username"),
+                        result.getString("Password"),
+                        result.getInt("Privileges"));
+            }
+
+        } catch (SQLException e) {
+            System.err.println(DatabaseManager.SQL_EXCEPTION + e.getMessage());
+        }
+        
+        return user;
+    }
+    
+    /**
+     * Delete user by its username
+     * @param username 
+     */
+    public void deleteUserByName(String username)
+    {
+        String sql = "DELETE FROM `User` WHERE `Username` = `" + username + "`";
+        this.databaseManager.insertQuery(sql);
     }
 }
